2e9125837c5278643dbe2a039dfc9900c377f9a8
Lucas Albertinazzi <lucas98albertinazzi@hotmail.com>
2026-09-23 14:59:59 -0300

    fix(web): preserva link público e atualiza ícones

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2784836439" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "665852570" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
